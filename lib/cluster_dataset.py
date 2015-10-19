aa4a4059334ff12b44464a451a989f99766f1025
Scott W Simpkins <ssimpkin@umn.edu>
2015-10-19 12:42:45 -0500

    Instead of leaving out rows/columns, I add noise where NaNs occur.
    This way, clustering can proceed with all rows/columns.

--- lib/cluster_dataset.py
+++ lib/cluster_dataset.py
@@ -16,14 +16,35 @@ import os
 def cluster(dataset):
 
     genes, conditions, matrix = dataset
+    print 'shape of original matrix:'
+    print matrix.shape
 
-    # Remove rows and columns that do not have any values - they kill the clustering process!
-    good_rows = np.nansum(matrix, axis = 1).astype(np.bool)
-    good_cols = np.nansum(matrix, axis = 0).astype(np.bool)
+    # Get the indices of NaNs in the matrix
+    nan_inds = np.isnan(matrix)
+    num_nans = np.sum(nan_inds)
 
-    genes = np.array(genes)[good_rows]
-    conditions = np.array(conditions)[good_cols]
-    matrix = matrix[np.ix_(good_rows, good_cols)]
+    # Replace the NaN values with extremely small noise values
+    # (noise has standard deviation of 1 million times less than the data)
+    np.random.seed(96857463)
+    data_sd = np.nanstd(matrix)
+    noise = np.random.randn(num_nans) * data_sd / 1e6
+    matrix[nan_inds] = noise
+
+    ## Remove rows and columns that do not have any values - they kill the clustering process!
+    ## print 'row_nansum: {}'.format(np.nansum(matrix, axis = 1))
+    ## print 'col_nansum: {}'.format(np.nansum(matrix, axis = 0))
+    #good_rows = np.nansum(matrix, axis = 1).astype(np.bool)
+    #good_cols = np.nansum(matrix, axis = 0).astype(np.bool)
+
+    #print 'number of good rows: {}'.format(np.nansum(good_rows))
+    #print 'number of good cols: {}'.format(np.nansum(good_cols))
+
+    #genes = np.array(genes)[good_rows]
+    #conditions = np.array(conditions)[good_cols]
+    #matrix = matrix[np.ix_(good_rows, good_cols)]
+
+    #print 'shape of good matrix:'
+    #print matrix.shape
 
     num_genes = len(genes)
     num_conds = len(conditions)
@@ -31,6 +52,26 @@ def cluster(dataset):
     # Compute distance matrices
     cols_dist = pdist(matrix.transpose(), 'cosine')
     rows_dist = pdist(matrix, 'cosine')
+
+    ## Get the names of rows and columns that have NaN dissimilarity values
+    #rows_dist_nan_inds_1, rows_dist_nan_inds_2 = [x[np.isnan(rows_dist)] for x in np.triu_indices(matrix.shape[0], 1)]
+    #cols_dist_nan_inds_1, cols_dist_nan_inds_2 = [x[np.isnan(cols_dist)] for x in np.triu_indices(matrix.shape[1], 1)]
+
+    #row_names_nan_dist_1, row_names_nan_dist_2 = genes[rows_dist_nan_inds_1], genes[rows_dist_nan_inds_2]
+    #col_names_nan_dist_1, col_names_nan_dist_2 = conditions[cols_dist_nan_inds_1], conditions[cols_dist_nan_inds_2]
+
+    ## And print out the rows (strains) and columns (conditions) with NaN dissimilarity values
+    #print "Strain pairs with NaN dissimilarity values:"
+    #for i, row_name_1 in enumerate(row_names_nan_dist_1):
+    #    print row_name_1, row_names_nan_dist_2[i]
+    #print ""
+    #
+    #print "Condition pairs with NaN dissimilarity values:"
+    #for i, col_name_1 in enumerate(col_names_nan_dist_1):
+    #    print col_name_1, col_names_nan_dist_2[i]
+    #print ""
+
+    
 
     # Cluster the matrix using fastcluster!
     print 'clustering columns...'
@@ -59,6 +100,9 @@ def cluster(dataset):
     # Create trees
     cols_tree = Tree(cols_nodes_list)
     rows_tree = Tree(rows_nodes_list)
+
+    # Add the NaNs back into the matrix, so it can be visualized properly
+    matrix[nan_inds] = np.nan
 
     # Create a giant text string so that the input data can be turned into a "record" object
     row1 = 'ORF\tNAME\tGWEIGHT\t' + '\t'.join(conditions)
