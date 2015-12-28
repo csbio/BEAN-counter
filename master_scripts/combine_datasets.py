fc91489e8b7619b7ed000f2c2ccc938f281b345f
Scott W Simpkins <ssimpkin@umn.edu>
2015-12-28 09:59:45 -0600

    Initial commit for what I believe is a functional combine_datsets script

---
+++ master_scripts/combine_datasets.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# This script takes in mulitple datasets and their corresponding sample tables and
+# spits out a combined dataset and a combined sample table. The only option
+# for this script is whether or not strains present in only one of the
+# datasets are kept as nans in the combined dataset or if they are excluded
+# entirely. The default is to only keep the intersect of the strains across the
+# entire dataset. If the user is to perform a batch correction to reduce effects
+# specific to each dataset, then one column in the sample table should clearly
+# define the dataset that each of the samples came from.
+import argparse
+import numpy as np, pandas as pd
+import networkx as nx
+import os, sys
+import gzip
+import cPickle
+
+def read_sample_table(tab_filename):
+
+    # Read everything in as a string, to prevent vexing
+    # number interpretation problems! Methods further down
+    # can coerce to different types.
+    tab = pd.read_table(tab_filename, dtype = 'S')
+    return tab
+
+def load_dataset(data_filename):
+
+    f = gzip.open(data_filename, 'rb')
+    barcodes, conditions, matrix = cPickle.load(f)
+    dataset = [np.array(barcodes), np.array(conditions), matrix]
+    f.close()
+    return dataset
+
+def dump_dataset(dataset, filename):
+
+    f = gzip.open(filename, 'wb')
+    cPickle.dump(dataset, f)
+    f.close()
+
+def filter_sample_table(sample_table, final_conditions):
+
+    all_conditions = np.array([tuple(row[['screen_name', 'expt_id']]) for i,row in sample_table.iterrows()])
+    rows_to_keep = [i for i, val in enumerate(all_conditions) if a_is_row_in_b(val, final_conditions)]
+    #print final_conditions
+    #print all_conditions
+    #print rows_to_keep
+
+    return sample_table.iloc[rows_to_keep]
+
+def a_is_row_in_b(a, b):
+
+    return np.any(np.all(a == b, axis = 1))
+
+def strain_match(a, b):
+
+    a_inds = []
+    b_inds = []
+    for i, x in enumerate(a):
+        if a_is_row_in_b(x, b):
+            a_inds.append(i)
+            b_inds.append(a.index(x))
+
+    return [a_inds, b_inds]
+
+# Define a function to get unique rows from a 2-D array (for getting
+# unique strains from a combined list). From user545424 on this
+# stackoverflow page: http://stackoverflow.com/questions/8560440/removing-duplicate-columns-and-rows-from-a-numpy-2d-array/8567929#8567929
+def unique_rows(a):
+    a = np.ascontiguousarray(a)
+    unique_a = np.unique(a.view([('', a.dtype)]*a.shape[1]))
+    return unique_a.view(a.dtype).reshape((unique_a.shape[0], a.shape[1]))
+
+def combine_datasets(datasets, all_strains):
+
+    # First, get all strains to include in the final matrix - this depends
+    # on if the value of all_strains is True (union of all strains) or
+    # False (intersect of all strains)
+    strains_tuple_list = [[tuple(z) for z in y] for y in [x[0] for x in datasets]]
+
+    if verbosity >= 2:
+        print strains_tuple_list
+
+    final_strains = set(strains_tuple_list[0])
+    if all_strains:
+        for i in range(1, len(strains_tuple_list)):
+            final_strains = final_strains.union(set(strains_tuple_list[i]))
+    else:
+        for i in range(1, len(strains_tuple_list)):
+            final_strains = final_strains.intersection(set(strains_tuple_list[i]))
+    final_strains = np.array(list(final_strains))
+
+    if verbosity >= 2:
+        print final_strains[0:10]
+        print len(final_strains)
+
+    # Now, get indices to reorder each matrix's rows to match the new order of the strains
+    old_strain_indices = []
+    new_strain_indices = []
+    for strains in strains_tuple_list:
+        strain_indices = strain_match(strains, final_strains)
+        old_strain_indices.append(np.array(strain_indices[0]))
+        new_strain_indices.append(np.array(strain_indices[1]))
+
+    # Create a large matrix that will be filled in, dataset by dataset
+    condition_lists = [x[1] for x in datasets]
+    final_conditions = np.vstack(condition_lists)
+  
+    if verbosity >= 2:
+        print "shapes of condition arrays before concatenation:"
+        for x in condition_lists:
+            print x.shape
+        print "shape of concatenated condition array: {}".format(final_conditions.shape)
+   
+    cond_lengths = np.array([x.shape[0] for x in condition_lists])
+    tot_conds = np.sum(cond_lengths)
+    end_cols = np.cumsum(cond_lengths)
+    start_cols = end_cols - cond_lengths
+    final_matrix = np.zeros((final_strains.shape[0], tot_conds))
+    final_matrix.fill(np.nan)
+
+    # Fill in the final matrix!
+    if verbosity >= 2:
+        print 'final_matrix shape: {}'.format(final_matrix.shape)
+    for i, dset in enumerate(datasets):
+        if verbosity >= 2:
+            print new_strain_indices[i].shape
+            print start_cols[i]
+            print end_cols[i]
+            print dset[2].shape
+            print old_strain_indices[i].shape
+        final_matrix[new_strain_indices[i], start_cols[i]:end_cols[i]] = dset[2][old_strain_indices[i]]
+
+    return [final_strains, final_conditions, final_matrix]
+
+def combine_sample_tables(tables):
+
+    # Iterate over all of the sample tables and get the column names
+    colname_list = [list(x.columns) for x in tables]
+
+    # This should be pretty simple using the append method for data frames...
+    combined_table = tables[0].append(tables[1:]).reset_index(drop = True)
+
+    return combined_table
+
+def main(dataset_list, sample_table_list, all_strains, output_folder, verbosity):
+
+    combined_dataset = combine_datasets(dataset_list, all_strains)
+
+    if verbosity >= 2:
+        print combined_dataset
+        print combined_dataset[0].shape
+        print combined_dataset[1].shape
+        print combined_dataset[2].shape
+
+    combined_sample_table = combine_sample_tables(sample_table_list)
+
+    if verbosity >= 2:
+        print combined_sample_table
+
+    dataset_filename = os.path.join(output_folder, 'combined_dataset.dump.gz')
+    table_filename = os.path.join(output_folder, 'combined_sample_table.txt')
+
+    dump_dataset(combined_dataset, dataset_filename)
+    combined_sample_table.to_csv(table_filename, sep = '\t', header = True, index = False)
+
+# If this is run from the command line (which is the intention), then run the rest of this script!
+if __name__ == '__main__':
+
+    # Get arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset_and_sample_table_files', metavar = 'DATASET_1 SAMPLE_TABLE_1 DATASET_2 SAMPLE_TABLE_2 ...', nargs = '+', help = 'The datasets and sample tables that are to be combined, in alternating order.')
+    parser.add_argument('--output_folder', help = 'The folder to which the resulting combined matrix and sample table are written.')
+    parser.add_argument('--all_strains', action = 'store_true', help = 'Add this flag if you want to keep all of the strains in the combined dataset, instead of the interesect of the strains. Strains present in one dataset but not the other will be represented as NaNs in the combined dataset.')
+    parser.add_argument('-v', '--verbosity', help = 'The level of verbosity printed to stdout. Ranges from 0 to 3, 1 is default.')
+
+    args = parser.parse_args()
+
+    # Take care of verbosity right away
+    if args.verbosity is None:
+        verbosity = 1
+    elif args.verbosity.isdigit():
+        verbosity = int(args.verbosity)
+    else:
+        verbosity = 1
+
+    # Get the data ready to rumble!
+    dataset_list = []
+    sample_table_list = []
+    for i, fname in enumerate(args.dataset_and_sample_table_files):
+        fname = os.path.abspath(fname)
+        assert os.path.isfile(fname), "File {} does not exist.".format(fname)
+        if i % 2 == 0:
+            assert fname.endswith('dump.gz'), "File {} is not a gzipped dump file".format(fname)
+            f = gzip.open(fname)
+            dataset = cPickle.load(f)
+            dataset_list.append(dataset)
+            f.close()
+        elif i % 2 == 1:
+            assert fname.endswith('.txt'), "File {} is not a text file".format(fname)
+            tab = read_sample_table(fname)
+            sample_table_list.append(tab)
+        else:
+            assert False, "something is very wrong if 'i % 2' returns something other than 0 or 1."
+
+    all_strains = args.all_strains
+
+    if verbosity >= 2:
+        print dataset_list
+        print sample_table_list
+        print args.all_strains
+
+    output_folder = args.output_folder
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
+
+    main(dataset_list, sample_table_list, all_strains, output_folder, verbosity)
+
