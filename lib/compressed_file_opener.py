b859984b656603f8d371ad18ed8f5f295a27a00d
Scott W Simpkins <ssimpkin@umn.edu>
2016-07-08 01:08:52 -0500

    Wrote a new implementation that is 1) original and 2) simpler.

--- lib/compressed_file_opener.py
+++ lib/compressed_file_opener.py
@@ -1,72 +1,37 @@
+#################################################################
+######  Copyright: Regents of the University of Minnesota  ######
+#################################################################
+
 # Function to open file with any type of compression, or none at all.
 # Assumes it is a text file if the "magic bits" do not match.
-# Currently all code courtesy of Ber from StackOverflow
+# Thanks to Ber from StackOverflow for the inspiration!
+# http://stackoverflow.com/questions/13044562/python-mechanism-to-identify-compressed-file-type-and-uncompress
 
-class CompressedFile (object):
-    magic = None
-    file_type = None
-    mime_type = None
-    proper_extension = None
-
-    def __init__(self, f):
-        # f is an open file or file like object
-        self.f = f
-        self.accessor = self.open()
-
-    @classmethod
-    def is_magic(self, data):
-        return data.startswith(self.magic)
-
-    def open(self):
-        return None
-
-import zipfile
-
-class ZIPFile (CompressedFile):
-    magic = '\x50\x4b\x03\x04'
-    file_type = 'zip'
-    mime_type = 'compressed/zip'
-
-    def open(self):
-        return zipfile.ZipFile(self.f)
-
-import bz2
-
-class BZ2File (CompressedFile):
-    magic = '\x42\x5a\x68'
-    file_type = 'bz2'
-    mime_type = 'compressed/bz2'
-
-    def open(self):
-        return bz2.BZ2File(self.f)
-
-import gzip
-
-class GZFile (CompressedFile):
-    magic = '\x1f\x8b\x08'
-    file_type = 'gz'
-    mime_type = 'compressed/gz'
-
-    def open(self):
-        return gzip.GzipFile(self.f)
-
-class TxtFile (CompressedFile):
-
-    file_type = 'plain'
-    mime_type = 'text/plain'
-
-    def open(self):
-        return file(self.f)
+import zipfile, bz2, gzip
 
 # factory function to create a suitable instance for accessing files
-def get_compressed_file(filename):
-    with file(filename, 'rb') as f:
-        start_of_file = f.read(1024)
-        # f.seek(0)
-        f.close()
-        for cls in (ZIPFile, BZ2File, GZFile):
-            if cls.is_magic(start_of_file):
-                return cls(filename)
-            
-        return TxtFile(filename)
+def get_compressed_file_handle(filename):
+    
+    # First, read in the very beginning of the file so we can check
+    # its magic bytes.
+    f = file(filename, 'rb')
+    file_start = f.read(1024)
+    f.close()
+
+    # Read in the file using the appropirate compressed file reader
+    # based on the magic bytes!
+
+    # First, gzip file (most common)
+    if file_start.startswith('\x1f\x8b\x08'):
+	return gzip.GzipFile(filename, 'rb')
+    # Now for bz2 files
+    elif file_start.startswith('\x42\x5a\x68'):
+	return bz2.BZ2File(filename, 'r')
+    # And zip files
+    elif file_start.startswith('\x50\x4b\x03\x04'):
+	return zipfile.Zipfile(filename, 'r')
+    # And if nothing matches, hopefully it's a text file!
+    else:
+	return file(filename, 'rt')
+
 
