ee66f34d5b5c6e71ca9eb6c6475ed8bb385ea377
Scott W Simpkins <ssimpkin@umn.edu>
2017-11-06 18:09:20 -0600

    Got a functional version of extra columns for dataset_to_text.py
    Actually had to do some speed optimization as the default pandas
    indexing was quite slow with tuple row keys. I think I achieved
    a ~100x speedup making my own dictionary index.

--- master_scripts/dataset_to_text.py
+++ master_scripts/dataset_to_text.py
@@ -45,7 +45,7 @@ def dump_dataset(dataset, filename):
     cPickle.dump(dataset, f)
     f.close()
 
-def main(dataset, dataset_file, table, val_name, strain_table_f, strain_columns, sample_table_f, condition_columns, verbosity):
+def main(dataset, dataset_file, table, val_name, strain_table_f, strain_columns, sample_table_f, condition_columns, output_file, verbosity):
 
     strains, conditions, matrix = dataset
 
@@ -54,10 +54,14 @@ def main(dataset, dataset_file, table, val_name, strain_table_f, strain_columns,
     if not os.path.isdir(out_folder):
         os.makedirs(out_folder)
 
-    # Here is where the script determines what modifications to make to the row/columns labels
-    if strain_table_f is not None:
-        strain_table = read_sample_table(strain_table_f)
-        custom_strains = clus_wrap.customize_strains(strains, strain_table, strain_columns)
+    ## Here is where the script determines what modifications to make to the row/column labels
+    #if strain_table_f is not None:
+    #    strain_table = read_sample_table(strain_table_f)
+    #    custom_strains = clus_wrap.customize_strains(strains, strain_table, strain_columns)
+    #
+    #if sample_table_f is not None:
+    #    sample_table = read_sample_table(sample_table_f)
+    #    custom_conditions = clus_wrap.customize_conditions(conditions, sample_table, condition_columns)
 
     # If the table boolean is not True, write out three fiies: matrix, rownames, and colnames
     if not table:
@@ -83,32 +87,68 @@ def main(dataset, dataset_file, table, val_name, strain_table_f, strain_columns,
     
     # If the table boolean is True, then reshape into a table and write everything out as one table
     else:
-        # Create containers for the rowname, colname, and value columns of the new table
-        Strain_IDs = []
-        Barcodes = []
-        screen_names = []
-        expt_ids = []
-        values = []
-        
-        # Iterate over the rows and columns of the matrix, and fill in the lists!
-        for i in range(matrix.shape[0]):
-            for j in range(matrix.shape[1]):
-                Strain_IDs.append(strains[i][0])
-                Barcodes.append(strains[i][1])
-                screen_names.append(conditions[j][0])
-                expt_ids.append(conditions[j][1])
-                values.append(matrix[i, j])
+        # Determine if custom information is to be added to the table and make sure column names check out
+        if strain_table_f is not None and strain_columns is not None:
+            strain_table = read_sample_table(strain_table_f)
+            strain_table_idx = {(strain_table.loc[i, 'Strain_ID'], strain_table.loc[i, 'Barcode']):i for i in range(strain_table.shape[0])}
+            strain_columns = strain_columns.split(',')
+            assert all([x in strain_table.columns for x in strain_columns]), "Not all of the specified columns are in the provided strain table ({})".format(';'.join(list(set(strain_columns) - set(strain_table.columns))))
+        else:
+            strain_columns = []
 
-        # Create the data frame!
+        if sample_table_f is not None and strain_columns is not None:
+            sample_table = read_sample_table(sample_table_f)
+            sample_table_idx = {(sample_table.loc[i, 'screen_name'], sample_table.loc[i, 'expt_id']):i for i in range(sample_table.shape[0])}
+            condition_columns = condition_columns.split(',')
+            assert all([x in sample_table.columns for x in condition_columns]), "Not all of the specified columns are in the provided sample table ({})".format(';'.join(list(set(condition_columns) - set(sample_table.columns))))
+        else:
+            condition_columns = []
+
+        # Determine identities of final table columns
         if val_name is None:
             val_name = 'value'
-        table = pd.DataFrame({'Strain_ID': Strain_IDs, 'Barcode': Barcodes, 'screen_name': screen_names, 'expt_id': expt_ids, val_name: values})
+        default_cols = ['Strain_ID', 'Barcode', 'screen_name', 'expt_id', val_name]
+        extra_strain_cols = []
+        extra_condition_cols = []
+        for col in strain_columns:
+            if col not in default_cols:
+                extra_strain_cols.append(col)
+        for col in condition_columns:
+            if col not in default_cols:
+                extra_condition_cols.append(col)
 
-        # Reorder the data frame columns!
-        table = table.reindex(columns = ['Strain_ID', 'Barcode', 'screen_name', 'expt_id', val_name])
+        # Create list of lists container for table-formatted data
+        final_cols = default_cols + extra_strain_cols + extra_condition_cols
+        final_data = [[] for x in final_cols]
+
+        # Iterate over the rows and columns of the matrix, and fill in the lists!
+        for i in range(matrix.shape[0]):
+            strain_idx = strain_table_idx[tuple(strains[i])]
+            for j in range(matrix.shape[1]):
+                k = 5
+                condition_idx = sample_table_idx[tuple(conditions[j])]
+                
+                final_data[0].append(strains[i][0])
+                final_data[1].append(strains[i][1])
+                final_data[2].append(conditions[j][0])
+                final_data[3].append(conditions[j][1])
+                final_data[4].append(matrix[i, j])
+                for col in extra_strain_cols:
+                    final_data[k].append(strain_table.loc[strain_idx, col])
+                    k += 1
+                for col in extra_condition_cols:
+                    final_data[k].append(sample_table.loc[condition_idx, col])
+                    k += 1
+
+        # Create the data frame!
+        table = pd.DataFrame(final_data).transpose()
+        table.columns = final_cols
 
         # Write the table out to file!
-        tab_fname = os.path.join(out_folder, 'data_table.txt.gz')
+        if output_file is None:
+            tab_fname = os.path.join(out_folder, 'data_table.txt.gz')
+        else:
+            tab_fname = output_file
         with gzip.open(tab_fname, 'wb') as tab_f:
             table.to_csv(tab_f, sep = '\t', header = True, index = False)
 
@@ -119,12 +159,13 @@ if __name__ == '__main__':
     # Get arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('dataset_file', help = 'The dataset to be reduced.')
-    parser.add_argument('--table', action = 'store_true', help = 'Add this flag if the output should be in table form, instead of a matrix plus row and column label files. Right now, the matrix prints out values with higher precision, but it should not make a difference for future computations.')
-    parser.add_argument('--strain_table', help = 'The strain barcode table used to interpret the strain barcodes (is in the "data" folder by default).')
-    parser.add_argument('--sample_table', help = 'The sample table corresponding to the dataset.')
-    parser.add_argument('--strain_columns', help = 'The columns from the barcode table to be included in the visualization.')
-    parser.add_argument('--condition_columns', help = 'The columns from the sample table to be included in the visualization.')
+    parser.add_argument('--table', action = 'store_true', help = 'Add this flag if the output should be in table form, instead of a matrix plus row and column label files. If --table is specified, the user can include additional information from the strain and sample tables to the output. For similar functionality with a matrix-formatted output, visualize_dataset.py provides output to CDT format which can be viewed in Java TreeView or a spreadsheet editor. Right now, the matrix prints out values with higher precision, but it should not make a difference for future computations.')
+    parser.add_argument('--strain_table', help = 'The strain barcode table used to interpret the strain barcodes (is in the "data" folder by default). Only used if --table is specified')
+    parser.add_argument('--sample_table', help = 'The sample table corresponding to the dataset. Only used if --table is specified.')
+    parser.add_argument('--strain_columns', help = 'Comma-delimited list of columns from the strain table to add to the output, if --table is specified.')
+    parser.add_argument('--condition_columns', help = 'Comma-delimited list of columns from the sample table to add to the output, if --table is specified.')
     parser.add_argument('--value_name', help = 'Optional argument to specify a name for the column that stores the matrix values')
+    parser.add_argument('--output_file', help = 'Optional argument to specify a different output file. Default is "data_table.txt.gz", deposited inside a folder named after the input dataset.')
     parser.add_argument('-v', '--verbosity', help = 'The level of verbosity printed to stdout. Ranges from 0 to 3, 1 is default.')
 
     args = parser.parse_args()
@@ -163,4 +204,4 @@ if __name__ == '__main__':
     table = args.table
     val_name = args.value_name
 
-    main(dataset, dataset_file, table, val_name, strain_table_f, strain_columns, sample_table_f, condition_columns, verbosity)
+    main(dataset, dataset_file, table, val_name, strain_table_f, strain_columns, sample_table_f, condition_columns, args.output_file, verbosity)
