20d5d0ef8518cccf977ac474c73ec659efcbd0ac
Scott W Simpkins <ssimpkin@umn.edu>
2017-12-14 12:32:59 -0600

    Set up detection of valid screen_config dirs, also
    enhanced interactive input format.

--- lib/parameters.py
+++ lib/parameters.py
@@ -4,6 +4,7 @@
 
 import os
 import textwrap
+from cg_common_functions import read_screen_config_params, read_barcode_table
 
 # Contains all default parameter values for filling in the
 # config file.
@@ -28,12 +29,28 @@ class Param:
         else:
             return self._is_valid_value(self.value)
 
+    def _write_help_string(self):
+        # Setup
+        opt_string = 'Type "o" to see options and "h" to print help again.'
+        #help_string = '{} {} (default: {})'.format(self.help, opt_string, self.value)
+        # Printing
+        print '\n' * 4
+        print '#' * (len(self.name) + 8)
+        print '##  {}  ##'.format(self.name)
+        print '#' * (len(self.name) + 8)
+        print ''
+        for x in textwrap.wrap(self.help, width = 60):
+            print x
+        print 'default: {}'.format(self.value)
+        print ''
+        print opt_string
+        print ''
+    
     # Add function to control input behavior here!
     def get_input(self):
-        opt_string = 'Type "o" to see options.'
-        help_string = '{} {} (default: {})'.format(self.help, opt_string, self.value)
-        for x in textwrap.wrap(help_string, width = 60):
-            print x
+
+        self._write_help_string()
+
         while True:
             y = raw_input('{}: '.format(self.name))
             if y == 'o':
@@ -42,43 +59,64 @@ class Param:
                         print self.options
                     elif isinstance(self.options, (list, tuple)):
                         for opt in self.options:
+                            print ''
+                            print 'Options for {}:'.format(self.name)
                             print opt
+                            print
                 else:
+                    print ''
                     print 'No pre-defined options for "{}".'.format(self.name)
+                    print ''
+            elif y == 'h':
+                self._write_help_string()
             else:
                 if self._is_valid_value(y):
                     self.value = y
                 if self.has_valid_value():
                     break
                 else:
+                    print ''
                     print 'No default value exists for "{}", so the user must supply one!'.format(self.name)
+                    print ''
 
 
 # Detection of valid screen_config directories in the
 # 'data/screen_config/' directory.
-#def is_valid_screen_config_dir(folder):
-#
-#    files = os.listdir(folder)
-#    if 'screen_config.yaml' in files:
-#        sc_params = parse_yaml(os.path.join(folder, 'screen_config.yaml'))
-#        if 'gene_barcode_file' in sc_params:
-#            try:
-#                read_barcode_table(sc_params['gene_barcode_file'])
-#                return True
-#            except AssertionError as e:
-#                pass
-#
-#    # If anything fails, return False
-#    return False
-#
-#def list_valid_screen_config_dirs():
-#    barseq_path = os.getenv('BARSEQ_PATH')
-#    assert barseq_path is not None, "'BARSEQ_PATH' environment variable is not set. Please consult the instructions for setting up BEAN-counter."
-#    sc_root_dir = os.path.join(barseq_path, 'data', 'screen_configs')
-#    sc_dirs = [os.path.join(sc_root_dir, x) for x in os.listdir(sc_root_dir) if os.path.isdir(x)]
-#    valid_sc_dirs = [x for x in sc_dirs if is_valid_screen_config_dir(x)]
-#
-#print list_valid_screen_config_dirs()
+def is_valid_screen_config_dir(folder):
+
+    files = os.listdir(folder)
+    print 'files:', files
+    if 'screen_config.yaml' in files:
+        sc_params = read_screen_config_params(os.path.join(folder, 'screen_config.yaml'))
+        if 'gene_barcode_file' in sc_params:
+            print 'gene barcode file:', sc_params['gene_barcode_file']
+            try:
+                read_barcode_table(sc_params['gene_barcode_file'])
+            except Exception as e:
+                print 'Assertion failed'
+            else:
+                print 'Assertion passed'
+
+            # If no exception was raised, then return True!
+            if 'e' not in locals():
+                print 'returning True...'
+                return True
+
+    # If anything fails, return False
+    return False
+
+def list_valid_screen_config_dirs():
+    barseq_path = os.getenv('BARSEQ_PATH')
+    assert barseq_path is not None, "'BARSEQ_PATH' environment variable is not set. Please consult the instructions for setting up BEAN-counter."
+    sc_root_dir = os.path.join(barseq_path, 'data', 'screen_configs')
+    print 'root dir:', sc_root_dir
+    sc_dirs = [x for x in os.listdir(sc_root_dir) if os.path.isdir(os.path.join(sc_root_dir, x))]
+    print 'screen config dirs:', sc_dirs
+    valid_sc_dirs = [x for x in sc_dirs if is_valid_screen_config_dir(os.path.join(sc_root_dir, x))]
+    print 'valid screen config dirs:', valid_sc_dirs
+    return valid_sc_dirs
+
+print list_valid_screen_config_dirs()
 
 # Definitions of file/folder locations for the config file
 # (and of the config file itself!)
@@ -125,9 +163,10 @@ loc_list.append(
                     'correctly parsing the sequencing data (screen_config.yaml), and 2) '\
                     'the file that maps barcodes '\
                     'to strains and their identifiers (barcodes.txt).',
-            options = None))
-#            options = list_valid_screen_config_dirs()))
+#            options = None))
+            options = list_valid_screen_config_dirs()))
 
-
+print loc_list[-1].name
+print loc_list[-1].options
 
 
