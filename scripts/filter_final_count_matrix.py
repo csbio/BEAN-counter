28abebbfb8362062649bee827d75356ec9587f5d
Scott W Simpkins <ssimpkin@umn.edu>
2018-02-16 13:45:08 -0600

    Bugfix for when no index tags are specified for automatic removal
    b/c default numpy array is float64 type

--- scripts/filter_final_count_matrix.py
+++ scripts/filter_final_count_matrix.py
@@ -131,7 +131,7 @@ def filter_dataset_for_index_tags(dataset, config_params):
 
     f = open(index_tag_corr_filename, 'rt')
     index_tags, correlations = cPickle.load(f)
-    index_tags_to_remove = np.array([index_tags[i] for i,corr in enumerate(correlations) if corr >= index_tag_correlation_cutoff])
+    index_tags_to_remove = np.array([index_tags[i] for i,corr in enumerate(correlations) if corr >= index_tag_correlation_cutoff], dtype = np.str)
     # print 'index_tags_to_remove:'
     # print '\n'.join(index_tags_to_remove) + '\n'
    
